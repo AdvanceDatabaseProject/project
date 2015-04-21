3e21e26034a2a05a6a96f2f7303b053f373ea6fa
junxinhe <hej77@students.rowan.edu>
2015-04-21 19:59:45 -0400

    commit3

--- public_html/js/addElement.js
+++ public_html/js/addElement.js
@@ -3,3 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+function addBook(){
+    var content=document.getElementById("more_book");
+    content.innerHTML=content.innerHTML+"Title: \n\
+        <span name='list_book'></span>\n\
+        Amount:\n\
+        <input type='text' name='stock' size='1'><br>";
+}
