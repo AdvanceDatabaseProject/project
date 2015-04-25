0bb0ae837bce6a6775d7b6328e5829e23ebe1315
junxinhe <hej77@students.rowan.edu>
2015-04-25 13:26:12 -0400

    4 commit

---
+++ public_html/js/add.js
@@ -0,0 +1,49 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var path = "http://elvis.rowan.edu/~mclaug67/adb/template.php?mode=add"
+
+function add(table) {
+    var header = path + "&table=" + table;
+    var string = header;
+    var genreflag = false;
+    var value;
+    var inp = Array.prototype.slice.call(document.getElementsByTagName("input"));
+    var sel = Array.prototype.slice.call(document.getElementsByTagName("select"));
+    var inputs = inp.concat(sel);
+    for (var i = 0; i < inputs.length; i++) {
+        var e = document.getElementById(inputs[i].getAttribute("id"));
+        if (e.getAttribute("id") === "genre" && table === "book") {
+            genreflag = true;
+        }  else if (e.value.trim() !== ""&&!e.disabled) {
+            string += "&" + e.getAttribute("id") + "=" + e.value;
+        }
+    }
+    if (string === header) {
+        alert("Please enter value(s) in fields in order to search");
+        return;
+    }
+    string = string.replace(/ /g, "%20");
+    getAddPhp(string);
+
+    if (genreflag) {
+        var ISBN=document.getElementById("ISBM").value;
+        value = document.getElementById("genre").value.replace(/ /g, "%20").split(",");
+        for(var i=0; i<value.length; i++){
+            addgenre(ISBN,value[i]);
+        }
+    }
+}
+
+function addgenre(ISBN,value){
+    var string=path+"&ISBN="+ISBN+"&genrename="+value;
+    getAddPhp(url);
+}
+
+function getAddPhp(url){
+    $.get(url, function (data) {
+
+    });
+}
---
+++ public_html/js/view.js
@@ -0,0 +1,65 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+var path = "http://elvis.rowan.edu/~mclaug67/adb/template.php?mode=view"
+
+function view(table, select) {
+    var header = path + "&table=" + table;
+    header += "&select=" + select;
+    var string = header;
+    var genreflag = false;
+    var value;
+    var inp = Array.prototype.slice.call(document.getElementsByTagName("input"));
+    var sel = Array.prototype.slice.call(document.getElementsByTagName("select"));
+    var inputs = inp.concat(sel);
+    for (var i = 0; i < inputs.length; i++) {
+        var e = document.getElementById(inputs[i].getAttribute("id"));
+        if (e.getAttribute("id") === "genre" && table === "book") {
+            genreflag = true;
+        }  else if (e.value.trim() !== "") {
+            string += "&" + e.getAttribute("id") + "=" + e.value;
+        }
+    }
+    if (string === header) {
+        alert("Please enter value(s) in fields in order to search");
+        return;
+    }
+    string = string.replace(/ /g, "%20");
+    getViewPhp(string);
+
+    if (genreflag) {
+        value = document.getElementById("ISBM").value;
+        viewgenre(value);
+    }
+}
+
+function viewgenre(ISBN) {
+    var string = path + "?table=genre";
+    string += "&select=*";
+    string += "&ISBN=" + ISBN;
+    //call PHP
+    alert(string);
+}
+
+function viewpublisher(publisherID) {
+    var string = path + "?table=genre";
+    string += "&select=*";
+    string += "&publisher_ID=" + publisherID;
+    //call PHP
+    alert(string);
+}
+
+function getViewPhp(url) {
+    $.get(url, function (data) {
+        var phpcontant = data.split(",");
+        for (var i = 0; i < phpcontant.length; i++) {
+            var e = phpcontant[i].split(":");
+            if (document.getElementById(e[0])) {
+                document.getElementById(e[0]).value = e[1];
+            }
+        }
+    });
+}
